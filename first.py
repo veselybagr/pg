709e23b09114f2a14df94c0a2d90c4e061167435
veselybagr <martin.skarda36@gmail.com>
2024-10-03 09:05:02 +0000

    poslední změna

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+def sudy_nebo_lichy(cislo):
+
+    if cislo % 2 == 0:
+        print(f"{cislo} je sude")
+    else:
+        print(f"{cislo} je liche")
+
+
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
